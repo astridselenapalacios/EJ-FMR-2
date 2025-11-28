9e9524d680a841958fc0379758d79f5c907b4c66
Astrid Palacios <astridselenapalacios@gmail.com>
2025-11-28 08:42:11 -0500

    Ejemplo de php

---
+++ server/save.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <h1>hola mundo desde PHP</h1>
+    <?php
+    $a = 100;
+    $b =2;
+    $c =$a * $b;
+    for ($i=0;$i < $c;$i++){
+        echo'<h1>hola mundo desde php </h1>';
+    }
+<body>
+    
+</body>
+</html>
